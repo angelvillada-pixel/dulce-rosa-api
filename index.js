581a536df4635c9a6b6ff0b6d7b3cdb78c50ed37
Angel Villada <angelvillada47@gmail.com>
2026-03-23 19:34:15 -0500

    Allow preview origin for QA

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const ALLOWED_ORIGINS = new Set([
   'https://dulcerosanails.pages.dev',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'http://localhost:4173',
+  'http://127.0.0.1:4173',
 ]);
 
 app.use(
